b9d6450bee95708aba4dcbae28e2bb5dac5eeca6
Dustin Dettmer <dustinpaystaxes@gmail.com>
2013-03-02 16:53:11 -0800

    Initial project

---
+++ Vintage/Vintage/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Vintage
+//
+//  Created by Dustin Dettmer on 3/2/13.
+//  Copyright (c) 2013 Dustin Dettmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
