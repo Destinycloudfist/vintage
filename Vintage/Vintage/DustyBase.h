144dec47e5d0322b775b095ce6d3da447ae6be2b
Dustin Dettmer <dustinpaystaxes@gmail.com>
2013-03-03 18:37:28 -0800

    Fix to make TransferViewController store the source key.

---
+++ Vintage/Vintage/DustyBase.h
@@ -0,0 +1,13 @@
+//
+//  DustyBase.h
+//  Vintage
+//
+//  Created by Dustin Dettmer on 3/3/13.
+//  Copyright (c) 2013 Dustin Dettmer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DustyBase : NSObject
+
+@end
---
+++ Vintage/Vintage/DustyBase.m
@@ -0,0 +1,13 @@
+//
+//  DustyBase.m
+//  Vintage
+//
+//  Created by Dustin Dettmer on 3/3/13.
+//  Copyright (c) 2013 Dustin Dettmer. All rights reserved.
+//
+
+#import "DustyBase.h"
+
+@implementation DustyBase
+
+@end
